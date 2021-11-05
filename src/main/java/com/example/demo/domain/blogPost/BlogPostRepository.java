4427776a3415ef4ffc26fb1b9a600cbefc6647f4
blerineberisha <69892200+blerineberisha@users.noreply.github.com>
2021-11-05 16:43:49 +0100

    updated BlogPostRepository --> added: extends JpaRepository

--- src/main/java/com/example/demo/domain/blogPost/BlogPostRepository.java
+++ src/main/java/com/example/demo/domain/blogPost/BlogPostRepository.java
@@ -1,4 +1,9 @@
 package com.example.demo.domain.blogPost;
 
-public interface BlogPostRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface BlogPostRepository  extends JpaRepository<BlogPost, UUID> {
+    
 }

b04824f946ecd733526ce27901794925d4fb9131
blerineberisha <69892200+blerineberisha@users.noreply.github.com>
2021-11-11 10:51:55 +0100

    added DeleteMapping, edited requests

--- src/main/java/com/example/demo/domain/blogPost/BlogPostController.java
+++ src/main/java/com/example/demo/domain/blogPost/BlogPostController.java
@@ -20,20 +20,24 @@ public class BlogPostController {
     }
 
     @GetMapping("")
-    public ResponseEntity<List<BlogPost>> findAll() {
-        List<BlogPost> blogPosts = blogPostService.getAllBlogPosts();
-        return ResponseEntity.ok().body(blogPosts);
+    public List<BlogPost> findAll() {
+        return this.blogPostService.getAllBlogPosts();
     }
 
     @PostMapping("/")
     //@PreAuthorize("hasRole('USER or ADMIN')")
-    public ResponseEntity<BlogPost> save(@RequestBody BlogPost blogPost) {
-        return ResponseEntity.ok().body(blogPostService.save(blogPost));
+    public BlogPost save(@RequestBody BlogPost blogPost) {
+        return this.blogPostService.save(blogPost);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<BlogPost> updateBlogPost(@PathVariable UUID id, @RequestBody BlogPost blogPost) {
-        return ResponseEntity.ok().body(blogPostService.updateBlogPost(blogPost, id));
+    public BlogPost updateBlogPost(@PathVariable UUID id, @RequestBody BlogPost blogPost) {
+        return this.blogPostService.updateBlogPost(blogPost, id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteBlogPost(@PathVariable UUID id){
+        this.blogPostService.deleteById(id);
     }
 
 }
